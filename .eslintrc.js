f052e1f3e03d37b602271b7082faff538834c000
Anjenette Luebrecht <41026190+aluebrecht@users.noreply.github.com>
2021-12-16 09:26:38 -0600

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  parser: '@babel/eslint-parser',
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    requireConfigFile: false,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  settings: {
+    react: {
+      version: '16.x',
+    },
+  },
+  rules: {
+    'react/prop-types': ['off'],
+    'react/no-unescaped-entities': ['error', { forbid: ['>', '}'] }],
+  },
+};
